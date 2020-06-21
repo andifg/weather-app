6d3cdb2f6fe3fd23419b0cba3d0f9c2752656bb5
andifg <45008199+andifg@users.noreply.github.com>
2020-06-21 10:55:46 +0200

    New Features Temp Max and Temp Min and minor changes

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -6,7 +6,7 @@ const forecast = (latidude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/d4c59549029b4e246c34a7357fa19a0d/' + encodeURIComponent(latidude) + ',' + encodeURIComponent(longitude) + '?units=si&lang=de'
 
     // Test case, to see the url for the request to darksky api
-    // console.log(url)
+    console.log(url)
 
     request({ url: url, json: true }, (error, { body }) => {
         if (error) {
@@ -16,7 +16,18 @@ const forecast = (latidude, longitude, callback) => {
             callback('Server sended error ' + body.error, undefined)
 
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+         var datehigh = new Date(body.daily.data[0].temperatureHighTime*1000+ body.offset*3600000)
+         var timehigh = datehigh.getUTCHours()+ ':' + addZero(datehigh.getUTCMinutes())
+         var datelow = new Date(body.daily.data[0].temperatureLowTime*1000 + body.offset*3600000)
+         var timelow = datelow.getUTCHours()+':'+ addZero(datelow.getUTCMinutes())
+
+         function addZero(i) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            return i;
+          }
+            callback(undefined,'Aktuell '+ body.currently.summary + '.'+ body.daily.data[0].summary + ' Die Temperatur beträgt aktuell ' + body.currently.temperature + ' Grad. Es ist eine ' + body.currently.precipProbability + '% Chance, dass es heute regnet.'+'Die maximale Temperatur tritt um ' + timehigh+ ' Uhr auf und beträgt '+ body.daily.data[0].temperatureHigh + ' Grad. Die minimale Temperatur tritt um '+ timelow + ' Uhr auf und beträgt ' + body.daily.data[0].temperatureLow + ' Grad.')
         }
 
     })
